f00910712282c17fe570000c4b00e123130f48b9
Tainara Paula <147462577+Tainaraoficial@users.noreply.github.com>
2025-11-16 18:03:48 +0000

    Nível intermediário finalizado.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Meu Primeiro Jogo
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
